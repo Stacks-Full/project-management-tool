b04287c9c7f40db6a85c1794f5e12ba7ad7b68be
HenryProEnterprise <henryproenterprises@gmail.com>
2025-12-05 00:04:35 -0500

    Created Pydantic Schemas for the user modual. this spacifily for Usercreate, Userlogin and Userpublic

---
+++ backend/app/api/schemas/schemas.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel,EmailStr
+
+#schemas for UserCreate, UserLogin and UserPublic
+
+
+
+#schema for userCreate
+class UserCreate(BaseModel):
+    full_name: str
+    email: EmailStr
+    username: str
+    hashed_password: str
+
+class UserResponse(BaseModel):
+    full_name: str
+    email: EmailStr
+    username: str
+
+    class Config:
+        from_attributes = True
+#sce
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str 
+
+
+class UserPublic(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+    full_name: str
+    user_token: str
+    is_online = bool
+    is_ofline = bool
+
+    class Config:
+        from_attributes = True
+    
