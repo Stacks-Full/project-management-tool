7e4db2b49c7f8d490578c7d3ccb9e49e3ef7dbc1
HenryProEnterprise <henryproenterprises@gmail.com>
2025-12-07 00:18:54 -0500

    Implemented changes for user schemas

--- backend/app/api/schemas/user_schemas.py
+++ backend/app/api/schemas/user_schemas.py
@@ -4,30 +4,22 @@ class UserCreate(BaseModel):
     full_name: str
     email: EmailStr
     username: str
-    hashed_password: str
-
-class UserResponse(BaseModel):
-    full_name: str
-    email: EmailStr
-    username: str
-
-    class Config:
-        from_attributes = True
+    password: str
 
 class UserLogin(BaseModel):
     email: EmailStr
     password: str 
-
 
 class UserPublic(BaseModel):
     id: int
     username: str
     email: EmailStr
     full_name: str
-    user_token: str
-    is_online = bool
-    is_ofline = bool
+    is_online: bool
 
     class Config:
         from_attributes = True
+
+class tokenresponse(BaseModel):
+    user_token: str
     
