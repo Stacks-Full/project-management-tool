5b3d5816bf8dd299e2241c6f230557a31e562df4
HenryProEnterprise <henryproenterprises@gmail.com>
2025-12-08 13:15:38 -0500

    added pydantic schemas

---
+++ backend/app/api/schemas/user_schemas.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel,EmailStr
+
+class UserCreate(BaseModel):
+    full_name: str
+    email: EmailStr
+    username: str
+    password: str
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str 
+
+class UserPublic(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+    full_name: str
+    is_online: bool
+
+    class Config:
+        from_attributes = True
+
+class tokenresponse(BaseModel):
+    user_token: str
